6fa37634f9527de704b75c491f1d105a8b6d9e8e
hna <ghnruf@naver.com>
2022-03-17 15:32:57 +0900

    Fix sol1

--- srcs/visualize.py
+++ srcs/visualize.py
@@ -1,6 +1,9 @@
+from cProfile import label
 import sys
 import matplotlib.pyplot as plt
 from TrainData import TrainData
+import seaborn as sns
+from pandas import DataFrame
 
 def show_groups_hist(data: dict, feature: str) -> None:
     plt.hist(list(filter(None, data["Ravenclaw"][feature])), bins = 10, alpha = 0.7, label="Ravenclaw", color="blue")
@@ -22,20 +25,43 @@ def sol0(train_data: TrainData) -> None:
   data = train_data.group_by("Hogwarts House")
   show_groups_hist(data, "Care of Magical Creatures")
 
-def sol1(data: dict) -> None:
+def show_all_scatter(data: dict) -> None:
+  features = list(data["Ravenclaw"].keys())[5:]
+  groups = list(data.keys())
+  for f1 in range(len(features)):
+    for f2 in range(f1 + 1, len(features)):
+      for group in groups:
+        # fig, (ax0, ax1) = plt.subplots(1, 2)
+        # ax0.hist(list(filter(None, data[features[f1]])), alpha=0.7)
+        # ax0.hist(list(filter(None, data[features[f2]])), alpha=0.7)
+        plt.scatter(data[group][features[f1]], data[group][features[f2]], label=group)
+      plt.xlabel(features[f1])
+      plt.ylabel(features[f2])
+      plt.legend()
+      plt.show()
+
+
+def sol1(train_data: TrainData) -> None:
   """
   What are the two features that are similar?
   """
-  features = list(data.keys())[6:]
-  for f1 in range(len(features)):
-    for f2 in range(f1 + 1, len(features)):
-      fig, (ax0, ax1) = plt.subplots(1, 2)
-      ax0.hist(list(filter(None, data[features[f1]])), alpha=0.7)
-      ax0.hist(list(filter(None, data[features[f2]])), alpha=0.7)
-      ax1.scatter(data[features[f1]], data[features[f2]])
-      plt.xlabel(features[f1])
-      plt.ylabel(features[f2])
-      plt.show()
+  data = train_data.group_by("Hogwarts House")
+  groups = list(data.keys())
+  # show_all_scatter(data)
+  for group in groups:
+    plt.scatter(data[group]["Potions"], data[group]["Care of Magical Creatures"], label = group)
+  plt.xlabel("Potions")
+  plt.ylabel("Care of Magical Creatures")
+  plt.legend()
+  plt.show()
+
+def sol2(train_data: TrainData) -> None:
+  """
+  From this visualization, what features are you going to use for your logistic regression?
+  """
+  df = DataFrame(train_data.data)
+  print(df)
+
 
 if __name__ == "__main__":
   if (len(sys.argv) != 2):
@@ -43,4 +69,4 @@ if __name__ == "__main__":
   train_data = TrainData()
   train_data.read_csv(sys.argv[1])
   train_data.convert_type()
-  sol1(train_data.data)
+  sol2(train_data)
